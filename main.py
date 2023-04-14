235894679127194bb3fa41d2954ec671012720e7
staz <m.kobialka@healthpharm.pl>
2023-04-14 08:23:54 +0200

    Wi change print

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Michal')
+    print_hi('Michal sdfsd')
+    print("Where")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
